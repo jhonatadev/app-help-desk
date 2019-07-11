f8b3b05297d00ccdf2f829360331ce1d1dfc382b
jhonatadev <xxxonata@gmail.com>
2019-07-11 12:30:12 -0300

    Consultando Chamados

--- consultar_chamado.php
+++ consultar_chamado.php
@@ -1,4 +1,22 @@
 <? require_once 'validador_acesso.php'; ?>
+<?php 
+
+  // Chamados
+  $chamados = array();
+
+  // Abri o arquivo.hd
+  $arquivo = fopen('arquivo.hd', 'r'); 
+
+  // Enquanto houver registros (linhas) a serem recuperados
+  while (!feof($arquivo)) { // Testa pelo fim de um arquivo
+  // Linhas
+  $registro = fgets($arquivo);
+  $chamados[] = $registro;
+ }
+
+  // Fechar o arquivo aberto
+  fclose($arquivo);
+?>
 
 <html>
   <head>
@@ -43,24 +61,25 @@
             </div>
             
             <div class="card-body">
-              
-              <div class="card mb-3 bg-light">
+              <? foreach ($chamados as $chamado) { ?>
+                
+              <?
+                $chamado_dados = explode('#', $chamado);
+
+                if (count($chamado_dados) < 3) {
+                  continue;
+                }
+              ?>              <div class="card mb-3 bg-light">
                 <div class="card-body">
-                  <h5 class="card-title">Título do chamado...</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
-                  <p class="card-text">Descrição do chamado...</p>
+
+                  <h5 class="card-title"><?=$chamado_dados[0]?></h5>
+                  <h6 class="card-subtitle mb-2 text-muted"><?=$chamado_dados[1]?></h6>
+                  <p class="card-text"><?=$chamado_dados[2]?></p>
 
                 </div>
               </div>
 
-              <div class="card mb-3 bg-light">
-                <div class="card-body">
-                  <h5 class="card-title">Título do chamado...</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
-                  <p class="card-text">Descrição do chamado...</p>
-
-                </div>
-              </div>
+              <? } ?>
 
               <div class="row mt-5">
                 <div class="col-6">
