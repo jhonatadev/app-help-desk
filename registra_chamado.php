224182305855aa26e97d1c16ed3aaa3279573a67
jhonatadev <xxxonata@gmail.com>
2019-07-11 04:09:42 -0300

    Registrando chamados :) uhuu

---
+++ registra_chamado.php
@@ -0,0 +1,21 @@
+<?php 
+	
+	print_r($_POST);
+
+	// Estamos trabalhando na montagem do texto
+	$titulo = str_replace('#', '-', $_POST['titulo']);	
+	$categoria = str_replace('#', '-', $_POST['categoria']);
+	$descricao = str_replace('#', '-', $_POST['descricao']);	
+
+	$texto = $titulo . '#' . $categoria . '#' . $descricao . PHP_EOL;
+
+	// Abrindo o arquivo
+	$arquivo = fopen('arquivo.hd', 'a');
+	// Escrevendo o texto
+	fwrite($arquivo, $texto);
+	// Fechando o arquivo
+	fclose($arquivo);
+
+	header('Location: abrir_chamado.php');
+
+?>
