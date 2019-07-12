9d48a761c64808808c1bf0d3b7e5b95f088a8407
jhonatadev <xxxonata@gmail.com>
2019-07-12 00:21:50 -0300

    Aplicando controle de perfil de usuário

--- registra_chamado.php
+++ registra_chamado.php
@@ -1,13 +1,13 @@
 <?php 
 	
-	print_r($_POST);
+	session_start();	
 
 	// Estamos trabalhando na montagem do texto
 	$titulo = str_replace('#', '-', $_POST['titulo']);	
 	$categoria = str_replace('#', '-', $_POST['categoria']);
 	$descricao = str_replace('#', '-', $_POST['descricao']);	
 
-	$texto = $titulo . '#' . $categoria . '#' . $descricao . PHP_EOL;
+	$texto = $_SESSION['id'] . '#' . $titulo . '#' . $categoria . '#' . $descricao . PHP_EOL;
 
 	// Abrindo o arquivo
 	$arquivo = fopen('arquivo.hd', 'a');
